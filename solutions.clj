5969858291e31b3fb5509dcc1d32a820fe61e77d
Pradeep Bishnoi <pradeep.bishnoi@lendingkart.com>
2020-02-23 22:21:40 +0530

    Solved till problem 21

---
+++ solutions.clj
@@ -0,0 +1,62 @@
+;; Problem 1
+true
+
+;;	Problem 2
+4
+
+;; Problem 3
+"HELLO WORLD"
+
+;; Problem 4
+:a :b :c
+
+;; Problem 5
+'(1 2 3 4)
+
+;; Problem 6
+:a :b :c 
+
+;; Problem 7
+[1 2 3 4]
+
+;; Problem 8
+#{:a :b :c :d}
+
+;; Problem 9
+2
+
+;; Problem 10
+20
+
+;; Problem 11
+{:b 2}
+
+;; Problem 12
+3
+
+;; Problem 13
+[20 30 40]
+
+;; Problem 14
+8
+
+;; Problem 15
+#(* % 2)
+
+;; Problem 16
+#(str "Hello, " % "!")
+
+;; Problem 17
+'(6 7 8)
+
+;; Problem 18
+(6 7)
+
+;; Problem 19
+#(first (reverse %))
+
+;; Problem 20
+#(second (reverse %))
+
+;; Problem 21
+(fn [x y] (get (vec x) y))
